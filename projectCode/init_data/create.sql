307c78d96795756d5c3f3c46131140b32fb38602
JacobLudwigson <jalu1895@gmail.com>
2023-11-07 16:37:21 -0700

    added sql file

--- projectCode/init_data/create.sql
+++ projectCode/init_data/create.sql
@@ -1,1 +1,21 @@
+CREATE TABLE users (
+    username VARCHAR(50) PRIMARY KEY,
+    password CHAR(60)
+);
+
+CREATE TABLE comments(
+    commentID SERIAL PRIMARY KEY,
+    comment VARCHAR(255),
+    eventID INT DISTINCT,
+    CONSTRAINT fk_username FOREIGN KEY(users) REFERENCES username
+);
+
+CREATE TABLE profile (
+    profileID SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    bio VARCHAR(255),
+    profile_pic_path VARCHAR(255), 
+    CONSTRAINT fk_username FOREIGN KEY(users) REFERENCES username
+);
 
