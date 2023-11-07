2c2672d3935fba6e9f71bc30612f38b6a2b759fb
JacobLudwigson <jalu1895@gmail.com>
2023-11-07 13:28:35 -0700

    Added initial directories + index.js

---
+++ projectCode/index.js
@@ -0,0 +1,182 @@
+// *****************************************************
+// <!-- Section 1 : Import Dependencies -->
+// *****************************************************
+
+const express = require('express'); // To build an application server or API
+const app = express();
+const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
+const bodyParser = require('body-parser');
+const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const bcrypt = require('bcrypt'); //  To hash passwords
+const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part B.
+
+// *****************************************************
+// <!-- Section 2 : Connect to DB -->
+// *****************************************************
+
+// database configuration
+const dbConfig = {
+  host: 'db', // the database server
+  port: 5432, // the database port
+  database: process.env.POSTGRES_DB, // the database name
+  user: process.env.POSTGRES_USER, // the user account to connect with
+  password: process.env.POSTGRES_PASSWORD, // the password of the user account
+};
+
+const db = pgp(dbConfig);
+
+// test your database
+db.connect()
+  .then(obj => {
+    console.log('Database connection successful'); // you can view this message in the docker compose logs
+    obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+    console.log('ERROR:', error.message || error);
+  });
+
+// *****************************************************
+// <!-- Section 3 : App Settings -->
+// *****************************************************
+
+app.set('view engine', 'ejs'); // set the view engine to EJS
+app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
+
+// initialize session variables
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// Authentication Middleware.
+// *****************************************************
+// <!-- Section 4 : API Routes -->
+// *****************************************************
+// TODO - Include your API routes here
+app.get('/', (req, res) => {
+    res.redirect('/login'); //this will call the /anotherRoute route in the API
+  });
+  
+// Register
+app.get('/register', function(req,res){
+    res.render('pages/register');
+});
+app.post('/register', async (req, res) => {
+    //hash the password using bcrypt library
+    const hash = await bcrypt.hash(req.body.password, 10);
+  
+    // To-DO: Insert username and hashed password into the 'users' table
+    var query = `INSERT INTO users (username, password) VALUES ('${req.body.username}' , '${hash}');`
+
+    db.any(query)
+        .then(function(){
+            res.redirect('/login');
+        })
+        .catch((err) => {
+            console.log(err)
+            res.render('pages/register', {
+              error : true,
+              message : "Failed to register",
+            }); 
+        });
+  });
+//login
+app.get('/login', function(req,res){
+    res.render('pages/login');
+});
+app.post('/login', (req, res) => {
+    
+    //save user details in session like in lab 8
+
+    var query = `SELECT * FROM users WHERE username = '${req.body.username}';`
+    
+    
+    // check if password from request matches with password in DB
+    // if(!match){
+    //     console.log(err);
+    //     res.redirect('/register');
+    // }
+    // else{
+    //     req.session.user = user;
+    //     req.session.save();
+    //     res.redirect('/discover');
+    // }
+    db.one(query)
+        .then(async function(user){
+            const match = await bcrypt.compare(req.body.password, user.password);
+            if (match){
+                //save user details in session like in lab 8
+                req.session.user = user;
+                req.session.save();
+                res.redirect('/discover')
+            }
+            else {
+                res.render('pages/login', {
+                    error: true,
+                    message : "Incorrect Username/Password",
+                });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            res.render('pages/register');
+        });
+});
+const auth = (req, res, next) => {
+    if (!req.session.user) {
+      // Default to login page.
+      return res.redirect('/login');
+    }
+    next();
+  };
+  
+  // Authentication Required
+app.use(auth);
+
+app.get('/logout', (req,res)=>{
+    req.session.destroy();
+    res.render('pages/login', {
+        error : false,
+        message: "Logged out successfully",
+    });
+})
+app.get('/discover',(req,res) => {
+
+    axios({
+        url: `https://app.ticketmaster.com/discovery/v2/events.json`,
+        method: 'GET',
+        dataType: 'json',
+        headers: {
+          'Accept-Encoding': 'application/json',
+        },
+        params: {
+          apikey: process.env.API_KEY,
+          keyword: "Alice in Chains", //you can choose any artist/event here
+          size: 10 // you can choose the number of events you would like to return
+        },
+    })
+    .then(results => {
+          console.log(results.data);
+          res.render('pages/discover', {events : results.data._embedded.events}); // the results will be displayed on the terminal if the docker containers are running // Send some parameters
+        })
+    .catch(error => {
+          console.log(error);
+          res.render('pages/discover', {events : []});
+        });
+});
+
+// *****************************************************
+// <!-- Section 5 : Start Server-->
+// *****************************************************
+// starting the server and keeping the connection open to listen for more requests
+app.listen(3000);
+console.log('Server is listening on port 3000');
