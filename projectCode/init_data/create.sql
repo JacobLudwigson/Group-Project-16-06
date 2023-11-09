db3f9fdea351f47e7d0838f52b4eee168e24c1c1
JacobLudwigson <jalu1895@gmail.com>
2023-11-09 09:41:41 -0700

    fixed docker problems&added menu&progress on profile.ejs

--- projectCode/init_data/create.sql
+++ projectCode/init_data/create.sql
@@ -6,8 +6,9 @@ CREATE TABLE users (
 CREATE TABLE comments(
     commentID SERIAL PRIMARY KEY,
     comment VARCHAR(255),
-    eventID INT DISTINCT,
-    CONSTRAINT fk_username FOREIGN KEY(users) REFERENCES username
+    eventID INT,
+    username VARCHAR(50),
+    CONSTRAINT fk_username FOREIGN KEY(username) REFERENCES users
 );
 
 CREATE TABLE profile (
@@ -16,6 +17,7 @@ CREATE TABLE profile (
     last_name VARCHAR(50),
     bio VARCHAR(255),
     profile_pic_path VARCHAR(255), 
-    CONSTRAINT fk_username FOREIGN KEY(users) REFERENCES username
+    username VARCHAR(50),
+    CONSTRAINT fk_username FOREIGN KEY(username) REFERENCES users
 );
 
